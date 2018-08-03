b9387e26b16ac174e5d79291d535885fe776a1a5
ydusaniuk <yaroslav.dusaniuk@gmail.com>
2018-08-03 15:47:36 +0300

    feat(toolbar): created toolbar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import styles from './Layout.css';
+import { Toolbar } from '../Navigation/Toolbar/Toolbar';
 
 export const Layout = (props) => (
   <React.Fragment>
-    <div>
-      Toolbar, SideDrawer, Backdrop
-    </div>
+    <Toolbar/>
     <main className={styles.Content}>
       {props.children}
     </main>
---
+++ src/components/Navigation/Toolbar/Toolbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Toolbar.css';
+
+export const Toolbar = props => (
+  <header className={styles.Toolbar}>
+    <div>Menu</div>
+    <div>Logo</div>
+    <nav>
+      ...
+    </nav>
+  </header>
+);
