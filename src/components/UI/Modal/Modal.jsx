9d9b66e1b4c174891285e98b12584966c1df42ec
ydusaniuk <yaroslav.dusaniuk@gmail.com>
2018-08-06 12:21:56 +0300

    feat(performance): added checkings for modal.jsx

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import styles from './Modal.css';
 import { Backdrop } from '../Backdrop/Backdrop';
 
-export const Modal = props => (
-  <React.Fragment>
-    <Backdrop visible={props.visible} clicked={props.modalClosed}/>
-    <div className={styles.Modal}
-         style={{
-           transform: props.visible ? 'translateY(0)' : 'translateY(-100vh)',
-           opacity: props.visible ? '1' : '0',
-         }}>
-      {props.children}
-    </div>
-  </React.Fragment>
-);
+export class Modal extends React.Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.visible !== this.props.visible;
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Backdrop visible={this.props.visible} clicked={this.props.modalClosed}/>
+        <div className={styles.Modal}
+             style={{
+               transform: this.props.visible ? 'translateY(0)' : 'translateY(-100vh)',
+               opacity: this.props.visible ? '1' : '0',
+             }}>
+          {this.props.children}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
