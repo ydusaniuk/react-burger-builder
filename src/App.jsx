9ae99883867aa0c80e7042b1b8c96b3a77b2ca89
ydusaniuk <yaroslav.dusaniuk@gmail.com>
2018-08-10 15:17:38 +0300

    feat(checkout): implemented routes for major parts

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import { Layout } from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 
 export const App = () => (
-  <div>
-    <Layout>
-      <BurgerBuilder/>
-      <Checkout/>
-    </Layout>
-  </div>
+  <BrowserRouter>
+    <div>
+      <Layout>
+        <Switch>
+          <Route path="/checkout" component={Checkout}/>
+          <Route path="/" component={BurgerBuilder}/>
+        </Switch>
+      </Layout>
+    </div>
+  </BrowserRouter>
 );
