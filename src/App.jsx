59b468c032d4632dc5a44359c1bd1407dc7b5f0f
ydusaniuk <yaroslav.dusaniuk@gmail.com>
2018-08-12 17:23:55 +0300

    feat(orders): implemented orders container with order component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Layout } from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 export const App = () => (
   <BrowserRouter>
@@ -11,6 +12,7 @@ export const App = () => (
       <Layout>
         <Switch>
           <Route path="/checkout" component={Checkout}/>
+          <Route path="/orders" component={Orders}/>
           <Route path="/" component={BurgerBuilder}/>
         </Switch>
       </Layout>
