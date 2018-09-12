a5ab8df86ea2f5118ebf709015d71f8cfa9dbfdb
ydusaniuk <yaroslav.dusaniuk@gmail.com>
2018-09-12 15:38:50 +0300

    fix(optimization): removed unnecessary updates of Input component

--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -1,54 +1,58 @@
 import React from 'react';
+
 import styles from './Input.css';
 
-const Input = (props) => {
-  const inputClasses = [styles.InputElement];
-  if (props.validation) {
-    if (props.validation.touched && !props.validation.isValid)
-      inputClasses.push(styles.Invalid);
+class Input extends React.Component {
+  shouldComponentUpdate(nextProps) {
+    return this.props.value !== nextProps.value;
   }
 
-  let inputEl = null;
+  createInputElement = (inputClasses) => {
+    switch (this.props.type) {
+      case 'textarea':
+        return <textarea className={inputClasses}
+                         value={this.props.value}
+                         onChange={this.props.onChange}
+                         placeholder={this.props.placeholder}/>;
 
-  switch (props.type) {
-    case 'textarea': {
-      inputEl = <textarea className={inputClasses.join(' ')}
-                          value={props.value}
-                          onChange={props.onChange}
-                          placeholder={props.placeholder}/>;
-      break;
+      case 'select':
+        return <select className={inputClasses}
+                       value={this.props.value}
+                       onChange={this.props.onChange}>
+          {
+            this.props.options.map(opt =>
+              <option key={opt.value} value={opt.value}>{opt.displayValue}</option>
+            )
+          }
+        </select>;
+
+      case 'input':
+      default: {
+        return <input className={inputClasses}
+                      value={this.props.value}
+                      onChange={this.props.onChange}
+                      type={this.props.elementType}
+                      placeholder={this.props.placeholder}/>;
+      }
+    }
+  };
+
+  render() {
+    const inputClasses = [styles.InputElement];
+    if (this.props.validation) {
+      if (this.props.validation.touched && !this.props.validation.isValid)
+        inputClasses.push(styles.Invalid);
     }
 
-    case 'select': {
-      inputEl = <select className={inputClasses.join(' ')}
-                        value={props.value}
-                        onChange={props.onChange}>
-        {
-          props.options.map(opt =>
-            <option key={opt.value} value={opt.value}>{opt.displayValue}</option>
-          )
-        }
-      </select>;
-      break;
-    }
+    let inputEl = this.createInputElement(inputClasses.join(' '));
 
-    case 'input':
-    default: {
-      inputEl = <input className={inputClasses.join(' ')}
-                       value={props.value}
-                       onChange={props.onChange}
-                       type={props.elementType}
-                       placeholder={props.placeholder}/>;
-      break;
-    }
+    return (
+      <div className={styles.Input}>
+        <label className={styles.Label}>{this.props.label}</label>
+        {inputEl}
+      </div>
+    )
   }
-
-  return (
-    <div className={styles.Input}>
-      <label className={styles.Label}>{props.label}</label>
-      {inputEl}
-    </div>
-  )
-};
+}
 
 export default Input;
